eeee24061333806e9436d40fce3f5fb0c0854b38
Saurav372 <105113447+Saurav372@users.noreply.github.com>
2024-07-16 16:27:49 +0000

    data variables

---
+++ 01/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 45593
+let accountEmail = "abc@xyz.com"
+var accountPassword = "Jaipur"
+accountCity = "Jaipur"
+
+// accountId = 2 Not Allowed
+
+accountEmail = "xyz@abc.com"
+
+accountPassword = "banglore"
+
+// console.log("accountEmail");
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
---
+++ 01/02_DataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as a newrer version
+
+// number => 2 to power 53
+// bigint => 
+// string => ""
+// Boolean => true/false
+// null => standlone value
+// undefined => 
+// Symbol => uniqueness
+
+
+// object => 
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
